986522d9bf96bb4ca92ae4092c61a484599ea37f
jayryuki3 <jayryuki3@gmail.com>
2026-03-08 23:18:00 -0700

    add App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import './global.css';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
+import { AuthProvider } from './src/hooks/useAuth';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <StatusBar style="light" />
+          <AppNavigator />
+          <Toast />
+        </AuthProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
